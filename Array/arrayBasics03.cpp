33f7ca66666be82780dde0672207b29336a2fb98
Gaurav Kumar <gaurav31120@gmail.com>
2024-04-05 15:55:32 +0530

    array functions

---
+++ Array/arrayBasics03.cpp
@@ -0,0 +1,19 @@
+// How to pass function inside an array;
+
+#include <iostream>
+using namespace std;
+
+void Fun(int arr[], int n)
+{
+    // cout<<sizeof(arr)<<endl;
+    for (int i = 0; i < 5; i++)
+        cout << arr[i] << " ";
+}
+int main()
+{
+
+    int arr[5] = {3, 2, 1, 6, 7};
+    // cout<<sizeof(arr)<<endl;
+    // calling a function
+    Fun(arr, 5);
+}
