abc267d974f8b1397a922f2720d23aa3131e4eec
Gaurav Kumar <gaurav31120@gmail.com>
2024-04-13 12:30:33 +0530

    separate -neg and pos

--- Array/separateNegAndPosNum.cpp
+++ Array/separateNegAndPosNum.cpp
@@ -7,24 +7,22 @@
 #include <iostream>
 using namespace std;
 
+// Two pointer approach - 1st type
+
 int main()
 {
     int arr[8] = {1, 74, -34, -43, 64, -13, 33, 31};
     int n = 8;
-    int i = 0, j = n - 1;
+    int j = 0;
 
-    while (i < j)
+    for (int i = 0; i < n; i++)
     {
         if (arr[i] < 0)
-            i++;
-        if (arr[j] > 0)
-            j--;
-        if (arr[i] > 0 && arr[j] < 0)
         {
             swap(arr[i], arr[j]);
+            j++;
         }
     }
-
     cout << "The req array is: " << endl;
     for (int i = 0; i < n; i++)
     {
@@ -32,6 +30,34 @@ int main()
     }
     return 0;
 }
+
+// Two pointer approach - 2nd type
+
+// int main()
+// {
+//     int arr[8] = {1, 74, -34, -43, 64, -13, 33, 31};
+//     int n = 8;
+//     int i = 0, j = n - 1;
+
+//     while (i < j)
+//     {
+//         if (arr[i] < 0)
+//             i++;
+//         if (arr[j] > 0)
+//             j--;
+//         if (arr[i] > 0 && arr[j] < 0)
+//         {
+//             swap(arr[i], arr[j]);
+//         }
+//     }
+
+//     cout << "The req array is: " << endl;
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout << arr[i] << " ";
+//     }
+//     return 0;
+// }
 
 void swap(int a, int b)
 {
