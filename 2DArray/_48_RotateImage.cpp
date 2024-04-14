552935d32f59dd7367d039aaf252859d2f9ac7ed
Gaurav Kumar <gaurav31120@gmail.com>
2024-04-14 16:57:30 +0530

    48. Rotate Image ----- 2D -- less complexity

--- 2DArray/_48_RotateImage.cpp
+++ 2DArray/_48_RotateImage.cpp
@@ -1,6 +1,10 @@
 // 48. Rotate Image ----- 2D
 // Rotate the image by 90 degrees (clockwise).
 
+// 1. Taking extra space ---- O(n*n) ---- SC -- O(n)
+// 2. With no extra space ---- O(n*n)
+
+// 2. With no extra space ---- O(n*n)
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -17,33 +21,67 @@ int main()
     // int m=mat.size();
     int i, j;
     // vector<vector<int>> ans(m);
+    cout << "The rotated array: " << endl;
 
     for (i = 0; i < n; i++)
     {
         for (j = 0; j < n; j++)
         {
-            ans[i][j] = mat[j][n - 1 - i];
-        }
-    }
 
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < n; j++)
-        {
-            cout << ans[i][j] << " ";
-        }
-        cout << endl;
-    }
-    cout << endl
-         << "---------------------" << endl;
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < n; j++)
-        {
-            cout << mat[i][j] << " ";
+            cout << mat[n - 1 - j][i] << " ";
         }
         cout << endl;
     }
 
     return 0;
 }
+
+//--------------------------------------------------------------------------
+
+// 1. Taking extra space ---- O(n*n) ---- SC -- O(n)
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// int main()
+// {
+//     int mat[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+//     int n = 3, m = 3;
+//     int ans[3][3] = {0};
+//     //  vector<int> v3 = {1, 2, 3, 4, 5};
+//     // vector<vector<int>> mat = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+//     // int n=mat.size();
+//     // int m=mat.size();
+//     int i, j;
+//     // vector<vector<int>> ans(m);
+
+//     for (i = 0; i < n; i++)
+//     {
+//         for (j = 0; j < n; j++)
+//         {
+//             ans[i][j] = mat[j][n - 1 - i];
+//         }
+//     }
+
+//     for (i = 0; i < n; i++)
+//     {
+//         for (j = 0; j < n; j++)
+//         {
+//             cout << ans[i][j] << " ";
+//         }
+//         cout << endl;
+//     }
+//     cout << endl
+//          << "---------------------" << endl;
+//     for (i = 0; i < n; i++)
+//     {
+//         for (j = 0; j < n; j++)
+//         {
+//             cout << mat[i][j] << " ";
+//         }
+//         cout << endl;
+//     }
+
+//     return 0;
+// }
