2cadec4da16a897c40f2ca79e5c2742c6f92485c
Gaurav Kumar <gaurav31120@gmail.com>
2024-04-15 20:51:30 +0530

    binary search

---
+++ Searching/binarSearch.cpp
@@ -0,0 +1,38 @@
+// Binary Search ---- TC - O(logn)
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[12] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    int n = 12;
+    int target = 9;
+
+    int mid, start, end;
+    start = 0, end = n - 1;
+
+    cout << "The target element is found at index: ";
+
+    while (start < end)
+    {
+        mid = (start + end) / 2;
+        if (arr[mid] == target)
+        {
+            cout << mid << endl;
+            return 0;
+        }
+        else if (arr[mid] > target)
+        {
+            mid--;
+            end = mid;
+        }
+        else
+        { // arr[mid] < target
+            mid++;
+            start = mid;
+        }
+    }
+
+    return 0;
+}
