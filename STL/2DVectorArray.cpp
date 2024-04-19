3fc155d08ad228aa56f3a983ff25c3e11b6025bc
Gaurav Kumar <gaurav31120@gmail.com>
2024-04-19 11:32:45 +0530

    2D Vector array Jacked array

--- STL/2DVectorArray.cpp
+++ STL/2DVectorArray.cpp
@@ -4,16 +4,47 @@
 #include <vector>
 using namespace std;
 
+// int main()
+// {
+//     vector<vector<int>> arr(5, vector<int>(10, 0));
+//     int n = arr.size();
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j < arr[i].size(); j++)
+//         {
+//             cout << arr[i][j] << " ";
+//         }
+//         cout << endl;
+//     }
+
+//     return 0;
+// }
+
+// Making custom array of differnt columns;
+// This is also known as Jacked array.
+
 int main()
 {
-    vector<vector<int>> arr(5, vector<int>(10, 0));
-    int n = arr.size();
+    vector< vector<int> > brr;
+    // int row = brr.size();
+    vector<int> vec1(10, 0);
+    vector<int> vec2(5, 1);
+    vector<int> vec3(7, 0);
+    vector<int> vec4(8, 1);
+    vector<int> vec5(20, 0);
+    
+    brr.push_back(vec1);
+    brr.push_back(vec2);
+    brr.push_back(vec3);
+    brr.push_back(vec4);
+    brr.push_back(vec5);
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < brr.size(); i++)
     {
-        for (int j = 0; j < arr[i].size(); j++)
+        for (int j = 0; j < brr[i].size(); j++)
         {
-            cout << arr[i][j] << " ";
+            cout << brr[i][j] << " ";
         }
         cout << endl;
     }
