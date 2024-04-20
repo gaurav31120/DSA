df8e2ea50d0da40581fbadbb3ada2edaee393f8d
Gaurav Kumar <gaurav31120@gmail.com>
2024-04-20 09:46:36 +0530

    corner cases

--- Searching/searchInSortedAndRotatedArray.cpp
+++ Searching/searchInSortedAndRotatedArray.cpp
@@ -55,13 +55,18 @@ int PivotElement(int arr[], int n)
     while (left <= right)
     {
         mid = left + (right - left) / 2;
+        // corner cases
+        if (left == right) // here, you will do mistake
+        {   // single element
+            return left;
+        }
 
-        if (arr[mid] < arr[mid - 1])
+        if (mid - 1 >= 0 && arr[mid] < arr[mid - 1]) // here, you will do mistake
         {
             pivot = mid - 1;
             break;
         }
-        else if (arr[mid] > arr[mid - 1])
+        else if (mid + 1 < n && arr[mid] > arr[mid + 1]) // here, you will do mistake
         {
             pivot = mid;
             break;
