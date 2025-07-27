bfb7a4246e46e38786aef9d3debdff6445796f5e
Gaurav Kumar <gaurav31120@gmail.com>
2025-07-27 15:22:24 +0530

    3 Taking inputs

---
+++ Array/Array notes/takingInputs.cpp
@@ -0,0 +1,14 @@
+// How to take input in an array
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[5];
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> arr[i];
+    }
+    return 0;
+}
