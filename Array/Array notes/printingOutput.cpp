63dc49b44167376e032cd5ea341b754b1fc95e93
Gaurav Kumar <gaurav31120@gmail.com>
2025-11-27 12:24:49 +0530

    printing output

---
+++ Array/Array notes/printingOutput.cpp
@@ -0,0 +1,17 @@
+// How to print array to output
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[5] = {5, 6, 7, 5, 3};
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
+
+// output
+// 5 6 7 5 3 
